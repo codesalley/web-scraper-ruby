0d4782a9b997a5c8d7297068037ae15af2aabdf2
juxsalley <juxsalley@gmail.com>
2021-02-19 13:38:13 +0000

    add search methods

--- lib/export.rb
+++ lib/export.rb
@@ -1,13 +1,38 @@
+require 'csv'
 class Export
   def initialize(data)
     @parsed_data = data
   end
 
   def search_by_css(search_node)
-    @parsed_data.css(search_node).text
+    data_arr = []
+    @parsed_data.css(search_node).each do |data|
+      data_arr << data
+    end
+    data_arr
   end
 
   def search_by_tags(search_node)
-    @parsed_data.xpath(search_node).text
+    data_arr = []
+    @parsed_data.xpath(search_node).each do |data|
+      data_arr << data
+    end
+    data_arr
+  end
+  def search_by_custom_input(input)
+    data_arr = []
+    @parsed_data.search(input).each do |data|
+      data_arr << data
+    end
+    data_arr
+  end
+  def export_to_cvs(data)
+    CSV.open('scrap.csv', 'w') do |csv|
+      csv << data
+      data.each do |data_i|
+        csv << data_i
+      end
+    end
+
   end
 end
