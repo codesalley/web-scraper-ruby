e85e270c7a365ab2ed2f306b95a49af3f9d75c8a
juxsalley <juxsalley@gmail.com>
2021-02-23 18:19:44 +0000

    linters error

--- lib/fetch.rb
+++ lib/fetch.rb
@@ -22,7 +22,10 @@ class FetchInternet
     response = HTTParty.get(@uri)
     data = parse_data(response.body)
     data.xpath('//table//tbody//tr//td//a//span').each_with_index do |_ele, index|
-      my_hash[data.xpath('//table//tbody//tr//td//a//span')[index].text.gsub(/\s+/, " ").strip] = data.css('div.truncate')[index.to_i].text.gsub(/\s+/, " ").strip if !data.css('div.truncate')[index.to_i].nil?
+      m_first = '//table//tbody//tr//td//a//span'
+      m_second = data.css('div.truncate')[index.to_i].text.gsub(/\s+/, ' ').strip
+
+      my_hash[data.xpath(m_first)[index].text.gsub(/\s+/, " ").strip] = m_second unless data.css('div.truncate')[index.to_i].nil?
     end
     my_hash
   end
